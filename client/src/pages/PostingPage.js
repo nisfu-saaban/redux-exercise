741d3d974a5995fe1aad9cc55b17f591c3cd05b1
nisfu-saaban <nisthy07@gmail.com>
2023-10-23 09:55:00 +0700

    cors

--- client/src/pages/PostingPage.js
+++ client/src/pages/PostingPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const PostingPage = () => {
+
+const {loginResult} = useSelector(state => state.UserReducer)
+
     return (
         <div className='p-3'>
 
--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
